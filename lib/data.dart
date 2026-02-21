f75cc32a15296626f3b1f596b77462bce4b963f4
farahalimohamed <sim.farahali3093@alexu.edu.eg>
2026-02-21 05:02:23 +0200

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,7 @@
+List<Map<String,dynamic>> ids = [
+  {"title": 'Build Flutter Apps Fast with Riverpod, Firebase, Hooks, and Freezed Architecture', "id": 'vrPk6LB9bjo',},
+  {"title": 'Flutter Instagram Stories', "id": 'ilX5hnH8XoI',},
+  {"title": 'Flutter Netflix Clone Responsive UI Tutorial | Web and Mobile', "id": 'rJKN_880b-M',},
+  {"title": 'Flutter Facebook Clone Responsive UI Tutorial | Web and Mobile', "id": 'HvLb5gdUfDE',},
+  {"title": 'Flutter Chat UI Tutorial | Apps From Scratch', "id": 'h-igXZCCrrc',}
+];
